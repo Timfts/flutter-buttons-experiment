9a30aaa71cdd456955d1c2bc1aa22785a1dee1d3
Italo Fontes <italo5123@hotmail.com>
2020-07-18 20:20:04 -0300

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Buttons experiment
+
+Just a project to explore different ways to create buttons
+
+
---
+++ android/app/src/main/kotlin/com/example/buttons_experiment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.buttons_experiment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
