dab290fb0628693f572a3654634bfbb38c58c27a
Italo Fontes <italo5123@hotmail.com>
2020-07-19 14:22:25 -0300

    :sparkles: added displaced button tap funcions

--- lib/widgets/buttons/displaced_button.dart
+++ lib/widgets/buttons/displaced_button.dart
@@ -5,16 +5,41 @@ import 'package:buttons_experiment/classes/button_style.dart';
 /// add touch animation
 /// add loading state
 
-class DisplacedButton extends StatelessWidget {
+class DisplacedButton extends StatefulWidget {
   final ButtonStyle style;
   final String text;
-
-  DisplacedButton({this.style, this.text = "Custom button", Key key})
+  final Function onTap;
+  DisplacedButton(
+      {this.style, this.text = "Custom button", this.onTap, Key key})
       : super(key: key);
+
+  @override
+  _DisplacedButtonState createState() => _DisplacedButtonState();
+}
+
+class _DisplacedButtonState extends State<DisplacedButton> {
+  Offset _backgroundOffset = Offset(5, 5);
+
+  void _handleButtonTap() {
+    print("tapped");
+  }
+
+  void _handleButtonTapDown(TapDownDetails details) {
+    setState(() {
+      _backgroundOffset = Offset(0, 0);
+    });
+  }
+
+  void _handleButtonTapUp(TapUpDetails details) {
+    setState(() {
+      _backgroundOffset = Offset(5, 5);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
-      constraints: BoxConstraints(maxWidth: 200),
+      constraints: BoxConstraints(maxWidth: 700),
       child: Stack(
         children: <Widget>[
           Positioned(
@@ -23,29 +48,26 @@ class DisplacedButton extends StatelessWidget {
             right: 0,
             bottom: 0,
             child: Transform.translate(
-              offset: Offset(5, 5),
+              offset: _backgroundOffset,
               child: Container(color: Colors.red),
             ),
           ),
-          Material(
-            color: Colors.transparent,
-            elevation: 0,
-            child: InkWell(
-              onTap: () => print("cenoura"),
-              onLongPress: () =>  print("long press"),
-              child: Container(
-                child: Text(
-                  this.text,
-                  style: TextStyle(
-                    color: this.style.textColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                  textAlign: TextAlign.center,
+          GestureDetector(
+            onTap: _handleButtonTap,
+            onTapUp: _handleButtonTapUp,
+            onTapDown: _handleButtonTapDown,
+            child: Container(
+              child: Text(
+                widget.text,
+                style: TextStyle(
+                  color: widget.style.textColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                decoration: BoxDecoration(border: Border.all(width: 2)),
+                textAlign: TextAlign.center,
               ),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              decoration: BoxDecoration(border: Border.all(width: 2)),
             ),
           ),
         ],
